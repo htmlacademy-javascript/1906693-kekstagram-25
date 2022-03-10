959006040eb433d89bd7af79ee535078a60a9980
Keks <keks@htmlacademy.ru>
2022-03-10 22:17:47 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,2 +1,4 @@
-import './data.js';
-import './miniatures.js';
+import { createImages } from './data.js';
+import { createPreviews } from './miniatures.js';
+const images = createImages();
+createPreviews(images);
