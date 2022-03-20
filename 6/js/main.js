7f02532d05b01e42de3bb244249134a6327948b2
Keks <keks@htmlacademy.ru>
2022-03-21 02:06:20 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,7 +1,7 @@
 import { createImages } from './data.js';
 import { createPreviews } from './create-previews.js';
-import { closeBigPicture } from './close-big-picture.js';
+import { initBigPicture } from './big-picture.js';
 
 const images = createImages();
 createPreviews(images);
-closeBigPicture();
+initBigPicture();
