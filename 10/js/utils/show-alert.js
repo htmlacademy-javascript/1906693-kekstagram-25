4750f80e3ebeaefc733fd435026a5546f283e851
Keks <keks@htmlacademy.ru>
2022-04-06 16:58:44 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/utils/show-alert.js
+++ 10/js/utils/show-alert.js
@@ -3,7 +3,6 @@ const ALERT_SHOW_TIME = 5000;
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
   alertContainer.classList.add('error');
-  alertContainer.style.zIndex = 100;
   alertContainer.textContent = message;
   document.body.append(alertContainer);
   setTimeout(() => {
