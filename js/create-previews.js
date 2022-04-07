6b0d9017d2f246d4a2858b10409dc7516af3c03b
SokolovMichael <unknownpersons@mail.ru>
2022-04-07 06:48:51 +0300

    Перламутровые пуговицы

--- js/create-previews.js
+++ js/create-previews.js
@@ -1,7 +1,20 @@
+import { initBigPicture } from './big-picture.js';
+import { getData } from './get-data-api.js';
+import { debounce } from './utils/debounce.js';
+
 const similarImagesBlock = document.querySelector('.pictures');
 const similarImagesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const imgFilters = document.querySelector('.img-filters');
+const defaultButton = imgFilters.querySelector('#filter-default');
+const randomButton = imgFilters.querySelector('#filter-random');
+const discussedButton = imgFilters.querySelector('#filter-discussed');
 
 const createPreviews = (images) => {
+  const pictures = document.querySelectorAll('.picture');
+  for (let i = 0; i < pictures.length; i++) {
+    pictures[i].remove();
+  }
+
   const similarImagesFragment = document.createDocumentFragment();
 
   images.forEach(({ url, likes, comments, id }) => {
@@ -14,6 +27,60 @@ const createPreviews = (images) => {
   });
 
   similarImagesBlock.appendChild(similarImagesFragment);
+  imgFilters.classList.remove('img-filters--inactive');
 };
 
-export { createPreviews };
+const initPressButtonDefaultImages = debounce(() => {
+  defaultButton.classList.add('img-filters__button--active');
+  randomButton.classList.remove('img-filters__button--active');
+  discussedButton.classList.remove('img-filters__button--active');
+  getData((images) => {
+    createPreviews(images);
+    initBigPicture(images);
+  });
+});
+
+const initPressButtonRandomImages = debounce(() => {
+  defaultButton.classList.remove('img-filters__button--active');
+  randomButton.classList.add('img-filters__button--active');
+  discussedButton.classList.remove('img-filters__button--active');
+  getData((images) => {
+    const randomImages = images.slice();
+
+    const shuffleArrayImages = (ArrayImages) => {
+      let j, temp;
+      for (let i = ArrayImages.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random()*(i + 1));
+        temp = ArrayImages[j];
+        ArrayImages[j] = ArrayImages[i];
+        ArrayImages[i] = temp;
+      }
+      return ArrayImages;
+    };
+
+    const randomArrayImages = shuffleArrayImages(randomImages).slice(0, 10);
+    createPreviews(randomArrayImages);
+    initBigPicture(randomArrayImages);
+  });
+});
+
+const initPressButtonDiscussedImages = debounce(() => {
+  defaultButton.classList.remove('img-filters__button--active');
+  randomButton.classList.remove('img-filters__button--active');
+  discussedButton.classList.add('img-filters__button--active');
+  const compareCommentsImages = (imageA, imageB) => imageB.comments.length - imageA.comments.length;
+  getData((images) => {
+    const discussedImages = images.slice();
+    discussedImages.sort(compareCommentsImages);
+    createPreviews(discussedImages);
+    initBigPicture(discussedImages);
+  });
+});
+
+const initPressButtonImagesFilters = () => {
+  defaultButton.addEventListener('click', initPressButtonDefaultImages);
+  randomButton.addEventListener('click', initPressButtonRandomImages);
+  discussedButton.addEventListener('click', initPressButtonDiscussedImages);
+};
+
+export { createPreviews, initPressButtonImagesFilters };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { createPreviews } from './create-previews.js';
+import { createPreviews, initPressButtonImagesFilters } from './create-previews.js';
 import { initBigPicture } from './big-picture.js';
 import { getData } from './get-data-api.js';
 import { initImgUpload, initImgUploadFormSubmit } from './validation-form.js';
@@ -8,5 +8,6 @@ getData((images) => {
   initBigPicture(images);
 });
 
+initPressButtonImagesFilters();
 initImgUpload();
 initImgUploadFormSubmit();
